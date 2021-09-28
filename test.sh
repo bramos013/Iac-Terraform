4d59f1c618f30df7fed2191b8d5ae46ace62b800
bramos013 <brunoramos013@gmail.com>
2021-09-28 18:40:35 -0300

    Realizando ajuste no test.sh

--- test.sh
+++ test.sh
@@ -6,5 +6,5 @@ status_http=$(curl -sw %{http_code} -o /dev/null ${dominio})
 echo "Domínio $dominio retornando status $status_http"
 
 if [ $status_http -eq 200 ]; then
-	echo "Passa na pipeline"	
+	echo "Teste OK"	
 fi
